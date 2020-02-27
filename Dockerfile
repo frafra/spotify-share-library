8ab1767441661d5606128e30c18472d5766e3fc7
Francesco Frassinelli <fraph24@gmail.com>
2020-02-27 19:07:11 +0100

    Use Starlette

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
 FROM python:3.7-slim
+LABEL maintainer="fraph24@gmail.com"
 
 WORKDIR /app
 ADD poetry.lock pyproject.toml ./
 RUN pip install poetry && \
-    poetry config virtualenvs.create false && \
-    poetry install
+    poetry install --no-root --no-dev
 ADD . .
 
 EXPOSE 8080
-CMD ["/usr/local/bin/python", "/app/spotify-share-library.py"]
-
+CMD ["/usr/local/bin/poetry", "run", "uvicorn", "api.index:app"]
--- api/index.py
+++ api/index.py
@@ -1,30 +1,39 @@
 #!/usr/bin/env python3
 
-from aiohttp import web
-from datetime import datetime
+from starlette.applications import Starlette
+from starlette.responses import RedirectResponse
+from starlette.routing import Route
+
 import os
 import spotify
 
 CLIENT_ID = os.environ['CLIENT_ID']
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
-REDIRECT_URI = os.environ['REDIRECT_URI']
+
+def get_redirect_uri(request):
+    redirect_uri = request.headers.get('x-now-deployment-url')
+    if redirect_uri:
+        redirect_uri = 'https://' + redirect_uri + '/callback'
+    else:
+        redirect_uri = os.environ['REDIRECT_URI']
+    return redirect_uri
 
 async def auth(request):
     async with spotify.Client(CLIENT_ID, CLIENT_SECRET) as client:
         url = client.oauth2_url(
-            redirect_uri=REDIRECT_URI,
+            redirect_uri=get_redirect_uri(request),
             scopes=['user-library-read', 'playlist-modify-public'])
-        return web.HTTPFound(url)
+        return RedirectResponse(url)
 
 async def callback(request):
     async with spotify.Client(CLIENT_ID, CLIENT_SECRET) as client:
         user = await spotify.User.from_code(
             client=client,
-            code=request.query['code'],
-            redirect_uri=REDIRECT_URI,
+            code=request.query_params['code'],
+            redirect_uri=get_redirect_uri(request),
             )
     display_name = user.display_name or user.id
-    public_library = "%s' Public library" % display_name
+    public_library = "%s's Public library" % display_name
     for playlist in await user.get_all_playlists():
         if playlist.name == public_library:
             break
@@ -32,14 +41,11 @@ async def callback(request):
         playlist = await user.create_playlist(public_library)
     tracks = await user.library.get_all_tracks()
     await playlist.replace_tracks(*tracks)
-    return web.HTTPFound(playlist.url)
+    return RedirectResponse(playlist.url)
 
-app = web.Application()
-app.add_routes([
-    web.get('/', auth),
-    web.get('/callback', callback),
-])
+routes = [
+    Route('/', endpoint=auth),
+    Route('/callback', endpoint=callback),
+]
 
-if __name__ == '__main__':
-    web.run_app(app)
-
+app = Starlette(os.environ.get('DEBUG', False), routes=routes)
